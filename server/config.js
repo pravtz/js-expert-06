6c7ad2b36de82f2b070d6e6f313b25bcda706198
pravtz <ederson.ufrgs@gmail.com>
2022-03-15 00:29:16 -0300

    create config base

---
+++ server/config.js
@@ -0,0 +1,29 @@
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+const root = join(currentDir, '../')
+const audioDirectory = join(root, 'audio')
+const publicDirectory = join(root, 'public')
+const songsDirectory = join(audioDirectory,'songs')
+const fxDirectory = join(audioDirectory,'fx')
+
+export default {
+    port: process.env.PORT || 3000,
+    dir: {
+        root,
+        audioDirectory,
+        publicDirectory,
+        songsDirectory,
+        fxDirectory
+
+    },
+    pages: {
+        homeHTML: 'home/index.html',
+        constrollerHTML: 'constroller/index.html',
+    },
+    location: {
+        home: '/home'
+    }
+}
